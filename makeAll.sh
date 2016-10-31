87238b559f35c42c706c2d1e937d7057773b5245
RichGraham <Richard.Graham@nottingham.ac.uk>
2016-10-31 11:43:27 +0000

    Created an install script; split profile into general-unix and mac-specific

---
+++ makeAll.sh
@@ -0,0 +1,8 @@
+#Make a grace templates folder (if it doesn't exist already)
+mkdir -p  ~/.grace/templates/
+ln -sf ~/source/utilityScripts/Default.agr ~/.grace/templates/
+
+ln -sf ~/source/utilityScripts/search ~/bin/
+ln -sf ~/source/utilityScripts/oldHistory ~/bin/
+ln -sf ~/source/utilityScripts/diskUse ~/bin/
+
