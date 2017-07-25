89bba5018af53efaf426d3a092e7dd7bc1177b44
RichGraham <Richard.Graham@nottingham.ac.uk>
2017-07-25 11:51:34 +0100

    Improved command line tips and a warning in the .py file

--- ipythToPython.sh
+++ ipythToPython.sh
@@ -2,26 +2,44 @@
 set -e
 #Check correct number of command line arguments
 if (( $# != 1 )); then
-   echo "Converts iPython notebooks to python scripts" >&2
-   echo "Usage  ipythToPython.sh [ipyth notebook]" >&2
-   echo "Example:  ipythToPython.sh SymmetricSqExp.ipynb" >&2
+    echo "Converts iPython notebooks to python scripts" >&2
+    echo " " >&2
+    echo "Notes: The opening line determines where the script is copied to (#source_location source/CO2-2Ar/iPy/)"
+    echo "       These lines are commented OUT 'testingname=dum_rInv-100.lhc #ipython_only'" >&2
+    echo "       These lines are comment IN #Python_Only testingname= sys.argv[1]' ">&2
+    echo " " >&2
+   echo "Usage  : ipythToPython.sh [ipyth notebook]" >&2
+   echo "Example: ipythToPython.sh SymmetricSqExp.ipynb" >&2
   exit 1
 fi
 
 #====Extract arguments====
 pythName=$1
 
+
+#====Main Script====
 #Strip the ipynb extension
 filename=$(basename "$pythName")
 filename="${filename%.*}"
 
 ipython nbconvert --to=python $pythName
 
+#Add a header warning
+currentDir=$(pwd)
+echo '#*********WARNING:********* Do not edit this directly, instead find and edit the ipython file'$currentDir'/'$pythName | cat - $filename.py > temp && mv temp $filename.py
+
 #Comment out the lines with the 'ipython_only' comment
 sed -i '' /#ipython_only/s/^/#/ $filename.py
 
 #Remove comments from 'Python_only lines
 sed -i '' 's/^#Python_Only //' $filename.py
+
+#Remove comments from 'Python_only lines
+cat $filename.py | grep '^#source_location' > dum
+sed -i '' 's/^#source_location //' dum
+location=$(cat dum)
+#sed -i '' 's/^#Python_Only //' $filename.py
+rm dum
 
 #Remove blank lines
 sed  -i '' '/^$/d'  $filename.py
@@ -32,3 +50,5 @@ sed -i -e "s/.*#[[:space:]]In.*/ /" $filename.py
 { echo '#!/usr/bin/env python'; cat  $filename.py; } >  dum
 mv dum  $filename.py
 chmod +x $filename.py
+echo Copied source to $HOME/$location/$filename.py
+cp $filename.py $HOME/$location
