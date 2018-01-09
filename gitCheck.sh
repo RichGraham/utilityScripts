2681dc4ee773338f609ffc31e8493b0c34ea2307
RichGraham <Richard.Graham@nottingham.ac.uk>
2018-01-09 11:48:07 +0000

    Added command to return status of all github repos in ~/source

---
+++ gitCheck.sh
@@ -0,0 +1,18 @@
+#!/bin/bash                                                                                                                                                                                        
+set -e #Quit if we get an error
+
+for d in ~/source/*/ ; do
+    cd $d
+    out1=$(git ls-files --modified --deleted --exclude-standard)
+    out2=$([ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
+sed 's/\// /g') | cut -f1) ] && echo "" || echo not up to date)
+    if [ -n "$out1$out2" ]
+    then
+	echo ============ $d ============
+	git status
+	echo ""
+	echo ""
+    fi
+    
+    #cd ..
+done
--- makeAll.sh
+++ makeAll.sh
@@ -8,6 +8,7 @@ ln -sf ~/source/utilityScripts/diskUse ~/bin/
 ln -sf ~/source/utilityScripts/.emacs ~/
 ln -sf ~/source/utilityScripts/timeDiff.sh ~/bin
 ln -sf ~/source/utilityScripts/ipythToPython.sh  ~/bin
+ln -sf ~/source/utilityScripts/gitCheck.sh  ~/bin
 
 
 git config --global core.excludesfile ~/source/utilityScripts/global_gitignore
