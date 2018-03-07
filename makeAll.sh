a5bbd51c217aacddd0a5aa41f6a7d936bcfffb6d
RichGraham <Richard.Graham@nottingham.ac.uk>
2018-03-07 10:46:50 +0000

    Script to sftp files to desktop dropbox folder

--- makeAll.sh
+++ makeAll.sh
@@ -9,6 +9,7 @@ ln -sf ~/source/utilityScripts/.emacs ~/
 ln -sf ~/source/utilityScripts/timeDiff.sh ~/bin
 ln -sf ~/source/utilityScripts/ipythToPython.sh  ~/bin
 ln -sf ~/source/utilityScripts/gitCheck.sh  ~/bin
+ln -sf ~/source/utilityScripts/sendToDesktop.sh  ~/bin
 
 
 git config --global core.excludesfile ~/source/utilityScripts/global_gitignore
