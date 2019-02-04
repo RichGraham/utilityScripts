0a0eb1e6174bf9f37cfdc883b4bd041107135e75
RichGraham <Richard.Graham@nottingham.ac.uk>
2019-02-04 10:13:35 +0000

    ipyth2Python wasn't removing blank and IN lines on a mac, now corrected

--- ipythToPython.sh
+++ ipythToPython.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+#set -e
 #Check correct number of command line arguments
 if (( $# != 1 )); then
     echo "Converts iPython notebooks to python scripts" >&2
@@ -26,7 +26,7 @@ ipython nbconvert --to=python $pythName
 
 #Add a header warning
 currentDir=$(pwd)
-echo '#*********WARNING:********* Do not edit this directly, instead find and edit the ipython file'$currentDir'/'$pythName | cat - $filename.py > temp && mv temp $filename.py
+echo '#*********WARNING:********* Do not edit this directly, instead copy the file elsewhere and edit or find and edit the ipython file'$currentDir'/ '$pythName | cat - $filename.py > temp && mv temp $filename.py
 
 #Comment out the lines with the 'ipython_only' comment
 sed -i '' /#ipython_only/s/^/#/ $filename.py
@@ -39,7 +39,7 @@ cat $filename.py | grep '^#source_location' > dum
 sed -i '' 's/^#source_location //' dum
 location=$(cat dum)
 #sed -i '' 's/^#Python_Only //' $filename.py
-rm dum
+#rm dum
 
 #Remove blank lines
 sed  -i '' '/^$/d'  $filename.py
@@ -52,3 +52,5 @@ mv dum  $filename.py
 chmod +x $filename.py
 echo Copied source to $HOME/$location/$filename.py
 cp $filename.py $HOME/$location
+#rm dum
+rm $filename.py-e 
