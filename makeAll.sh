7b4cb1e7c72c1b872f1ee49e32e7f96e4fc40d0b
Richard S. Graham <RichGraham@users.noreply.github.com>
2022-07-27 19:48:32 +0100

    Added pylint options

--- makeAll.sh
+++ makeAll.sh
@@ -6,6 +6,7 @@ ln -sf ~/source/utilityScripts/search ~/bin/
 ln -sf ~/source/utilityScripts/oldHistory ~/bin/
 ln -sf ~/source/utilityScripts/diskUse ~/bin/
 ln -sf ~/source/utilityScripts/.emacs ~/
+ln -sf ~/source/utilityScripts/.pylintrc ~/
 ln -sf ~/source/utilityScripts/timeDiff.sh ~/bin
 ln -sf ~/source/utilityScripts/ipythToPython.sh  ~/bin
 ln -sf ~/source/utilityScripts/gitCheck.sh  ~/bin
