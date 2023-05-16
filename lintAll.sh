4924eddb5b2ec83cc8162da85f12128dced5a35f
Richard S. Graham <RichGraham@users.noreply.github.com>
2023-05-16 14:57:57 +0100

    Improved output message for script that criticises number of lines in a class

--- lintAll.sh
+++ lintAll.sh
@@ -5,13 +5,13 @@ do
     if [[ ! "$file" =~ ^Test ]]   && [[ ! "$file" =~ ^Property_Test ]] && [[ ! "$file" =~ ^SupportTests ]]
     then
 	echo  $file
-	
-	minimumsize=200
+
+	maximumsize=200
 	actualsize=$(wc -l <"$file")
-	if [ $actualsize -ge $minimumsize ]; then
-	    echo !!!!! $actualsize is probably too many lines. Does the class do too many things? !!!!!
+	if [ $actualsize -ge $maximumsize ]; then
+	    echo !!!!! $actualsize is over $maximumsize lines. Does the class do too many things? !!!!!
 	fi
-	
+
 	pylint  --score=y --reports=n $file | grep 'Your code has been'
 	echo ' '
     fi
